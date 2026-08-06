46cf45bef6901b023a0ec49fb9cf8b78d04cf53b
Tayfun <tayfuna766@gmail.com>
2026-08-06 05:35:53 +0300

    chore: refresh hero cache after truck visibility fix

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ if ($html === false) {
 }
 
 $assets = <<<'HTML'
-  <link rel="stylesheet" href="css/hero-fix.css?v=20260806-4">
-  <link rel="stylesheet" href="css/hero-base-fix.css?v=20260806-4">
-  <script src="js/hero-fix.js?v=20260806-4" defer></script>
-  <script src="js/hero-base-fix.js?v=20260806-4" defer></script>
+  <link rel="stylesheet" href="css/hero-fix.css?v=20260806-6">
+  <link rel="stylesheet" href="css/hero-base-fix.css?v=20260806-6">
+  <script src="js/hero-fix.js?v=20260806-6" defer></script>
+  <script src="js/hero-base-fix.js?v=20260806-6" defer></script>
 HTML;
 
 if (strpos($html, '</head>') !== false) {
